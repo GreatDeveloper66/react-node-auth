88971ceb09ae761bca205fa896cf9491840fe812
GreatDeveloper66 <ashaffe@gmail.com>
2023-12-28 07:24:29 -0600

    Update AuthContext with new imports

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -2,39 +2,47 @@ import { createContext, useState, useContext, useEffect } from "react";
 
 const AuthContext = createContext();
 const useAuth = () => useContext(AuthContext);
-export { AuthProvider, useAuth };
+
+// ... (other imports)
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+  
     const login = (userData) => {
-        setUser(userData);
-        localStorage.setItem("user", JSON.stringify(userData));
+      setUser(userData);
+      localStorage.setItem("user", JSON.stringify(userData));
     };
+  
     const logout = () => {
-        setUser(null);
-        localStorage.removeItem("user");
+      setUser(null);
+      localStorage.removeItem("user");
     };
-
+  
     const register = (userData) => {
-        setUser(userData);
-        localStorage.setItem("user", JSON.stringify(userData));
+      setUser(userData);
+      localStorage.setItem("user", JSON.stringify(userData));
     };
-    const useEffect = () => {
-        const user = localStorage.getItem("user");
-        if (user) {
-            setUser(JSON.parse(user));
-        }
-    }
+  
+    useEffect(() => {
+      const storedUser = localStorage.getItem("user");
+      if (storedUser) {
+        setUser(JSON.parse(storedUser));
+      }
+    }, []); // Empty dependency array to run the effect only once
+  
     const authContextValue = {
-        user,
-        login,
-        logout,
+      user,
+      login,
+      logout,
+      register,
     };
+  
     return (
-        <AuthContext.Provider value={authContextValue}>
-            {children}
-        </AuthContext.Provider>
+      <AuthContext.Provider value={authContextValue}>
+        {children}
+      </AuthContext.Provider>
     );
-   
-
-    };
+  };
+  
+  export { AuthProvider, useAuth };
+  
