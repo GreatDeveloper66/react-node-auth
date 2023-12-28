0d43d4f88e957dee407c5daff730a6d07571e557
GreatDeveloper66 <ashaffe@gmail.com>
2023-12-28 07:24:06 -0600

    Update App.js with new structure

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,26 @@
  * @returns The App component is being returned.
  */
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
+import RegistrationPage from './pages/RegistrationPage';
 
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
+      <Router>
         <Routes>
-          <Route path="/login" component={LoginPage} />
-          <Route path="/dashboard" component={DashboardPage} />
-          <Route path="/register" component={RegistrationPage} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/register" element={<RegistrationPage />} />
           {/* Add more routes as needed */}
         </Routes>
-      </BrowserRouter>
+      </Router>
     </AuthProvider>
   );
 }
 
 export default App;
+
