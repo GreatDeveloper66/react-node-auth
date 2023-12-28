7e256d1f035fd981c0b490facbb6e591300605d3
GreatDeveloper66 <ashaffe@gmail.com>
2023-12-28 06:32:45 -0600

    add signup, signin, siginout

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,4 @@
 const express = require('express');
 const router = express.Router();
 
-//Authentication Routes
-const { signup, signin, signout } = require('../controllers/auth');
-
 module.exports = router;
