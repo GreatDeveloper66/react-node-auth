5713528e4ec1ac68257a1dc381727a61e1009e5d
GreatDeveloper66 <ashaffe@gmail.com>
2023-12-29 06:42:50 -0600

    add password and email validation

--- client/src/pages/RegistrationPage.js
+++ client/src/pages/RegistrationPage.js
@@ -6,8 +6,13 @@ import { container, heading, form, label, input, button, linkText } from '../css
 const RegistrationPage = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [passwordValidation, setPasswordValidation] = useState(null);
+    const [confirmPasswordValidation, setConfirmPasswordValidation] = useState(null);
+    const [emailValidation, setEmailValidation] = useState(null);
     const { register } = useAuth();
     const navigate = useNavigate();
+
 
     const handleRegistration = async () => {
         try {
@@ -17,14 +22,46 @@ const RegistrationPage = () => {
             email,
             password
         }; // Replace with actual user data
-        register(userData);
+            register(userData);
             navigate('/dashboard', {state: {user: userData, logOut: false}});
         } catch (error) {
         // Handle login error
             console.error(error);
         }
     };
+
+    const handlePasswordChange = (e) => {
+        const passwordValue = e.target.value;
+        setPassword(passwordValue);
+        const validPassword = validatePassword(passwordValue);
+        setPasswordValidation(validPassword);
+    }
+
+    const handleEmailChange = (e) => {
+        const emailValue = e.target.value;
+        setEmail(emailValue);
+        const validEmail = validateEmail(emailValue);
+        setEmailValidation(validEmail);
+    }
+
+    const handlePasswordConfirmationChange = (e) => {
+        const passwordValue = e.target.value;
+        setConfirmPassword(passwordValue);
+        const validPassword = validateConfirmPassword(passwordValue);
+        setConfirmPasswordValidation(validPassword);
+    }
+
+    const validateEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+    const validatePassword = (password) => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,20}$/.test(password);
     
+    const validateConfirmPassword = (password) => password === password;
+
+    const getPasswordMask = (length) => '*'.repeat(length);
+
+    const validUser = () => emailValidation && passwordValidation && confirmPasswordValidation;
+    
+
     return (
         <div style={container}>
         <h2 style={heading}>Registration Page</h2>
@@ -33,24 +70,26 @@ const RegistrationPage = () => {
             <input
                 type="email"
                 value={email}
-                onChange={(e) => setEmail(e.target.value)} 
-                style={input}     
+                onChange={(e) => handleEmailChange(e)} 
+                style={{ ...input, color: emailValidation === null ? 'black' : emailValidation ? 'green' : 'red' }}     
             />
             <label style={label}>Password:</label>
             <input
                 type="password"
                 value={password}
-                onChange={(e) => setPassword(e.target.value)}   
-                style={input}
+                display={getPasswordMask(password.length)}
+                onChange={(e) => handlePasswordChange(e)}   
+                style={{ ...input, color: passwordValidation === null ? 'black' : passwordValidation ? 'green' : 'red' }}
             />
             <label style={label}>Confirm Password:</label>
             <input
                 type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}    
-                style={input}       
+                value={confirmPassword}
+                display={getPasswordMask(confirmPassword.length)}
+                onChange={(e) => handlePasswordConfirmationChange(e)}    
+                style={{ ...input, color: confirmPasswordValidation === null ? 'black' : confirmPasswordValidation ? 'green' : 'red' }}       
             />
-            <button onClick={handleRegistration} style={button}>Register</button>
+            <button onClick={handleRegistration} style={button} disabled={!validUser()}>Register</button>
             </form>
         <p style={linkText}>
             Already have an account? <Link to="/">Login here</Link>
